3e7ce6af0edcde60b04b07a31f66f1ae93b558de
sanjeewa03 <sanjeewasenarathna03@gmail.com>
2019-05-11 18:06:24 +0530

    message backend added

--- project/sever/server.js
+++ project/sever/server.js
@@ -16,11 +16,11 @@ app.use(jwt());
 // api routes
 app.use('/users', require('./modelsControlersAndServices/users/users.controller'));
 app.use('/hotel', require('./modelsControlersAndServices/hotels/hotel.controller'));
-<<<<<<< HEAD
+
 app.use('/message',require('./modelsControlersAndServices/messages/messages.controller'));
-=======
+
 app.use('/safari',require('./modelsControlersAndServices/safari/safari.model'));
->>>>>>> c92b7a839f7d1cff029ea81d2f6d5a9751a4ed6f
+
 // global error handler
 app.use(errorHandler);
 
